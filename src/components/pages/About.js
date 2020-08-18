28259f7509dfa5dd176d721fa20518cac41665dc
Derek Hassan <dhass421@gmail.com>
2020-08-18 14:42:07 -0400

    Completed tutorial

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>THis is the Todo List app v1.0.0. It is part of the React crash course by Brad Traversy.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
